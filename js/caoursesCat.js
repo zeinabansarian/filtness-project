702c2213a6535d3e67e93fa4035cd6afc96c61f4
Zeinab <zeinabansarian6280@gmail.com>
2024-09-13 11:15:18 +0330

    add images loaded to courses cat and home page

--- js/caoursesCat.js
+++ js/caoursesCat.js
@@ -1,89 +1,98 @@
-let progresBarContainer = document.querySelector(".progresBarContainer")
-let sliders = document.querySelectorAll(".swiper-slide")
-for (let i = 1; i < sliders.length; i++) {
-  const element = sliders[i];
-  let newprogressBar = document.createElement("div")
-  newprogressBar.classList.add("progresBar")
-  let newSpan = document.createElement("span")
+$('.coursesCat').imagesLoaded( function() {
+
+setTimeout(() => {
   
-  newprogressBar.appendChild(newSpan)
-  progresBarContainer.appendChild(newprogressBar)
-}
-
-
-const slider = document.getElementById("js-cta-slider");
-let progresBar = document.querySelectorAll(".progresBar");
-const interleaveOffset = 0.75;
-let progressList = document.querySelectorAll(".progressContainer ul li")
-progressList[0].classList.add("activeBar")
-let lastIndex = 0;
-const mySwiper = new Swiper(slider, {
-  loop: false,
-  direction: "vertical",
-  autoplay: true,
-  speed: 1000,
-  grabCursor: false,
-  watchSlidesProgress: true,
-  mousewheelControl: true,
-  mousewheel: true,
-
-  on: {
-    progress: function () {
-      let swiper = this;
-
-      for (let i = 0; i < swiper.slides.length; i++) {
-        let slideProgress = swiper.slides[i].progress;
-        let innerOffset = swiper.height * interleaveOffset;
-        let innerTranslate = slideProgress * innerOffset;
-
-        //TweenMax.set(swiper.slides[i], {
-        //skewY: `${innerTranslate*0.025}deg`,
-        //});
-        TweenMax.set(swiper.slides[i].querySelector(".slide-inner"), {
-          y: innerTranslate,
-        });
-      }
+  let progresBarContainer = document.querySelector(".progresBarContainer")
+  let sliders = document.querySelectorAll(".swiper-slide")
+  for (let i = 1; i < sliders.length; i++) {
+    const element = sliders[i];
+    let newprogressBar = document.createElement("div")
+    newprogressBar.classList.add("progresBar")
+    let newSpan = document.createElement("span")
+    
+    newprogressBar.appendChild(newSpan)
+    progresBarContainer.appendChild(newprogressBar)
+  }
+  
+  
+  const slider = document.getElementById("js-cta-slider");
+  let progresBar = document.querySelectorAll(".progresBar");
+  const interleaveOffset = 0.75;
+  let progressList = document.querySelectorAll(".progressContainer ul li")
+  progressList[0].classList.add("activeBar")
+  let lastIndex = 0;
+  const mySwiper = new Swiper(slider, {
+    loop: false,
+    direction: "vertical",
+    autoplay: true,
+    speed: 1000,
+    grabCursor: false,
+    watchSlidesProgress: true,
+    mousewheelControl: true,
+    mousewheel: true,
+  
+    on: {
+      progress: function () {
+        let swiper = this;
+  
+        for (let i = 0; i < swiper.slides.length; i++) {
+          let slideProgress = swiper.slides[i].progress;
+          let innerOffset = swiper.height * interleaveOffset;
+          let innerTranslate = slideProgress * innerOffset;
+  
+          //TweenMax.set(swiper.slides[i], {
+          //skewY: `${innerTranslate*0.025}deg`,
+          //});
+          TweenMax.set(swiper.slides[i].querySelector(".slide-inner"), {
+            y: innerTranslate,
+          });
+        }
+      },
+      touchStart: function () {
+        let swiper = this;
+        for (let i = 0; i < swiper.slides.length; i++) {
+          swiper.slides[i].style.transition = "";
+        }
+      },
+      setTransition: function (speed) {
+        let swiper = this;
+        for (let i = 0; i < swiper.slides.length; i++) {
+          swiper.slides[i].style.transition = speed + "ms";
+          swiper.slides[i].querySelector(".slide-inner").style.transition =
+            speed + "ms";
+        }
+      },
+  
+      slideChangeTransitionStart: function () {
+        if (this.activeIndex>lastIndex) {
+          progresBar[this.activeIndex-1].classList.add("activeBar");
+         lastIndex= this.activeIndex;
+         
+        }else{
+          progresBar[this.activeIndex].classList.remove("activeBar");
+          lastIndex= this.activeIndex
+  
+        }
+        // let count = 1 / (this.slides.length - 1);
+        $(".progressContainer ul li").removeClass("activeBar")
+        progressList[this.activeIndex].classList.add("activeBar")
+        
+        if (this.activeIndex == 0) {
+  
+        } else {
+          progresBar[this.activeIndex-1].classList.add("activeBar");
+        }
+      },
     },
-    touchStart: function () {
-      let swiper = this;
-      for (let i = 0; i < swiper.slides.length; i++) {
-        swiper.slides[i].style.transition = "";
-      }
-    },
-    setTransition: function (speed) {
-      let swiper = this;
-      for (let i = 0; i < swiper.slides.length; i++) {
-        swiper.slides[i].style.transition = speed + "ms";
-        swiper.slides[i].querySelector(".slide-inner").style.transition =
-          speed + "ms";
-      }
-    },
+  });
+  
+  $(".progressContainer ul li").click(function () {
+    mySwiper.slideTo($(this).index());
+    $(".progressContainer ul li").removeClass("activeBar");
+    $(this).addClass("activeBar");
+  });
+}, 1000);
 
-    slideChangeTransitionStart: function () {
-      if (this.activeIndex>lastIndex) {
-        progresBar[this.activeIndex-1].classList.add("activeBar");
-       lastIndex= this.activeIndex;
-       
-      }else{
-        progresBar[this.activeIndex].classList.remove("activeBar");
-        lastIndex= this.activeIndex
-
-      }
-      // let count = 1 / (this.slides.length - 1);
-      $(".progressContainer ul li").removeClass("activeBar")
-      progressList[this.activeIndex].classList.add("activeBar")
-      
-      if (this.activeIndex == 0) {
-
-      } else {
-        progresBar[this.activeIndex-1].classList.add("activeBar");
-      }
-    },
-  },
+  
 });
 
-$(".progressContainer ul li").click(function () {
-  mySwiper.slideTo($(this).index());
-  $(".progressContainer ul li").removeClass("activeBar");
-  $(this).addClass("activeBar");
-});
--- js/home.js
+++ js/home.js
@@ -1,153 +1,158 @@
-if (window.innerWidth > 800) {
-  const text = new SplitType("#target", { types: "lines" });
-}
-let answers = document.querySelectorAll(".section7 .card .answer");
-let cards = document.querySelectorAll(".section7 .card");
-let numbers = document.querySelectorAll(".section7 .card .number span");
-let imgList = document.querySelectorAll(".imgList .imgS");
-let inList = document.querySelectorAll(".inList ul li");
-let inList1 = document.querySelectorAll(".list section");
-
-cards[0].classList.add("activeFq");
-answers[0].style.display = "block";
-
-numbers.forEach((element, index) => {
-  if (index < 9) {
-    element.innerHTML = `0${index + 1}`;
-  } else {
-    element.innerHTML = `${index + 1}`;
+$('.home').imagesLoaded( function() {
+setTimeout(() => {
+  
+  if (window.innerWidth > 800) {
+    const text = new SplitType("#target", { types: "lines" });
   }
-});
-imgList.forEach((element, index) => {
-  element.style.top = `${(index / inList1.length) * 3 + inList1.length}em`;
-});
-
-inList.forEach((element, index) => {
-  element.addEventListener("mouseenter", function (params) {
-    $(".goal").css("opacity", "0");
-    $(".peri").css("opacity", "0");
-
-    $(".goal").css("display", "none");
-    $(".peri").css("display", "none");
-
-    element.querySelector(".goal").style.display = "block";
-    element.querySelector(".peri").style.display = "block";
-    setTimeout(() => {
-      element.querySelector(".goal").style.opacity = "1";
-      element.querySelector(".peri").style.opacity = "1";
-    }, 220);
-
-    if (window.innerWidth > 650) {
+  let answers = document.querySelectorAll(".section7 .card .answer");
+  let cards = document.querySelectorAll(".section7 .card");
+  let numbers = document.querySelectorAll(".section7 .card .number span");
+  let imgList = document.querySelectorAll(".imgList .imgS");
+  let inList = document.querySelectorAll(".inList ul li");
+  let inList1 = document.querySelectorAll(".list section");
+  
+  cards[0].classList.add("activeFq");
+  answers[0].style.display = "block";
+  
+  numbers.forEach((element, index) => {
+    if (index < 9) {
+      element.innerHTML = `0${index + 1}`;
+    } else {
+      element.innerHTML = `${index + 1}`;
+    }
+  });
+  imgList.forEach((element, index) => {
+    element.style.top = `${(index / inList1.length) * 3 + inList1.length}em`;
+  });
+  
+  inList.forEach((element, index) => {
+    element.addEventListener("mouseenter", function (params) {
+      $(".goal").css("opacity", "0");
+      $(".peri").css("opacity", "0");
+  
+      $(".goal").css("display", "none");
+      $(".peri").css("display", "none");
+  
+      element.querySelector(".goal").style.display = "block";
+      element.querySelector(".peri").style.display = "block";
+      setTimeout(() => {
+        element.querySelector(".goal").style.opacity = "1";
+        element.querySelector(".peri").style.opacity = "1";
+      }, 220);
+  
+      if (window.innerWidth > 650) {
+        $(".inList ul li .inner").removeClass("activeList");
+        element.querySelector(".inner").classList.add("activeList");
+        $(".imgList .imgS").css("transform", "translateX(-3em)");
+        $(".imgList .imgS").css("opacity", "0");
+        imgList[index].style.opacity = "1";
+        imgList[index].style.transform = "translateX(0)";
+      }
+    });
+    element.addEventListener("mouseleave", function (params) {
+      // setTimeout(() => {
+  
+      $(".goal").css("opacity", "0");
+      $(".peri").css("opacity", "0");
+  
+      $(".goal").css("display", "none");
+      // }, 220);
+      $(".peri").css("display", "none");
+  
       $(".inList ul li .inner").removeClass("activeList");
-      element.querySelector(".inner").classList.add("activeList");
       $(".imgList .imgS").css("transform", "translateX(-3em)");
       $(".imgList .imgS").css("opacity", "0");
-      imgList[index].style.opacity = "1";
-      imgList[index].style.transform = "translateX(0)";
-    }
+    });
   });
-  element.addEventListener("mouseleave", function (params) {
-    // setTimeout(() => {
-
-    $(".goal").css("opacity", "0");
-    $(".peri").css("opacity", "0");
-
-    $(".goal").css("display", "none");
-    // }, 220);
-    $(".peri").css("display", "none");
-
-    $(".inList ul li .inner").removeClass("activeList");
-    $(".imgList .imgS").css("transform", "translateX(-3em)");
-    $(".imgList .imgS").css("opacity", "0");
-  });
-});
-
-cards.forEach((element) => {
-  element.addEventListener("click", function (params) {
-    // console.log(element.getAttribute("data-open"));
-    let flag = element.classList.contains("activeFq");
-    if (flag) {
-      console.log("true");
-      element.classList.remove("activeFq");
-      setTimeout(() => {
-        element.querySelector(".answer").style.display = "none";
-      }, 500);
-    } else {
-      console.log("false");
-
-      element.querySelector(".answer").style.display = "block";
-      setTimeout(() => {
-        element.classList.add("activeFq");
-      }, 100);
-    }
-  });
-});
-
-let circles = document.querySelectorAll(".circle");
-circles.forEach((element) => {
-  gsap.to(element.querySelector(".back"), {
-    scrollTrigger: {
-      trigger: element,
-      start: "top 70%",
-      end: "top 50%",
-    },
-    scale: 1,
-  });
-  gsap.to(element.querySelector("span"), {
-    scrollTrigger: {
-      trigger: element,
-      start: "top 70%",
-      end: "top 50%",
-      onEnter: () => (element.querySelector("span").style.opacity = "1"),
-      onLeaveBack: () => (element.querySelector("span").style.opacity = "0"),
-    },
-  });
-});
-
-
-let callcuBmi = document.querySelector(".callcuBmi")
-
-callcuBmi.addEventListener("click" , function (event) {
-  let inputs = document.querySelectorAll(".bmiForm input")
-  let msg = document.querySelector(".msg")
-  let msgP = document.querySelector(".msg p")
-  let heightt = document.querySelector(".heightt")
-  let weightt = document.querySelector(".weightt");
-  let bmiInner = document.querySelector(".bmiInner")
-  event.preventDefault();
-  let gender = document.querySelector(".gender")
-  console.log(gender.value);
-  inputs.forEach(element => {
-    if (element.value=="") {
-      msg.style.display="block"
-      msgP.innerHTML="لطفا ورودی ها را کامل کنید.";
-      
-      setTimeout(() => {
-        
-        msg.style.display="none"
-      }, 2000);
-    }else if(gender.value==""){
-      
-      msg.style.display="block"
-      msgP.innerHTML="لطفا جنسیت را انتخاب کنید.";
-      
-      setTimeout(() => {
-        
-        msg.style.display="none"
-      }, 2000);
-    }
-    else{
-      let bmi = Math.round((Number(weightt.value)/Math.pow(Number(heightt.value/100),2))*100)/100
-      bmiInner.innerHTML=bmi
-    }
-    
-  });
-
-
   
-})
-
-
-
-const player = new Plyr('.section3 video', {captions: {active: true}});
-window.player = player;
+  cards.forEach((element) => {
+    element.addEventListener("click", function (params) {
+      // console.log(element.getAttribute("data-open"));
+      let flag = element.classList.contains("activeFq");
+      if (flag) {
+        console.log("true");
+        element.classList.remove("activeFq");
+        setTimeout(() => {
+          element.querySelector(".answer").style.display = "none";
+        }, 500);
+      } else {
+        console.log("false");
+  
+        element.querySelector(".answer").style.display = "block";
+        setTimeout(() => {
+          element.classList.add("activeFq");
+        }, 100);
+      }
+    });
+  });
+  
+  let circles = document.querySelectorAll(".circle");
+  circles.forEach((element) => {
+    gsap.to(element.querySelector(".back"), {
+      scrollTrigger: {
+        trigger: element,
+        start: "top 70%",
+        end: "top 50%",
+      },
+      scale: 1,
+    });
+    gsap.to(element.querySelector("span"), {
+      scrollTrigger: {
+        trigger: element,
+        start: "top 70%",
+        end: "top 50%",
+        onEnter: () => (element.querySelector("span").style.opacity = "1"),
+        onLeaveBack: () => (element.querySelector("span").style.opacity = "0"),
+      },
+    });
+  });
+  
+  
+  let callcuBmi = document.querySelector(".callcuBmi")
+  
+  callcuBmi.addEventListener("click" , function (event) {
+    let inputs = document.querySelectorAll(".bmiForm input")
+    let msg = document.querySelector(".msg")
+    let msgP = document.querySelector(".msg p")
+    let heightt = document.querySelector(".heightt")
+    let weightt = document.querySelector(".weightt");
+    let bmiInner = document.querySelector(".bmiInner")
+    event.preventDefault();
+    let gender = document.querySelector(".gender")
+    console.log(gender.value);
+    inputs.forEach(element => {
+      if (element.value=="") {
+        msg.style.display="block"
+        msgP.innerHTML="لطفا ورودی ها را کامل کنید.";
+        
+        setTimeout(() => {
+          
+          msg.style.display="none"
+        }, 2000);
+      }else if(gender.value==""){
+        
+        msg.style.display="block"
+        msgP.innerHTML="لطفا جنسیت را انتخاب کنید.";
+        
+        setTimeout(() => {
+          
+          msg.style.display="none"
+        }, 2000);
+      }
+      else{
+        let bmi = Math.round((Number(weightt.value)/Math.pow(Number(heightt.value/100),2))*100)/100
+        bmiInner.innerHTML=bmi
+      }
+      
+    });
+  
+  
+    
+  })
+  
+  
+  
+  const player = new Plyr('.section3 video', {captions: {active: true}});
+  window.player = player;
+}, 1000);
+});
